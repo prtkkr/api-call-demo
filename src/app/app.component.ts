730cc48065317021c3d0a308ae01155b7430e6a8
Pratik Kumar <pratikkumarsbg@gmail.com>
2021-08-14 14:30:19 +0530

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { UsersService } from './users.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'api-call-demo';
+  aData: any;
+
+  constructor(private user: UsersService) {
+    this.user.getData().subscribe(data=>{
+      console.warn(data);
+      this.aData = data;
+    })
+  }
 }
